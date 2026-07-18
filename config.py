3aef8ecaefebd9891f17be0b753fbcd218693854
Digital Pen Tech <digitalpentech@gmail.com>
2026-07-18 10:59:51 +0100

    chore: update database URI for persistent disk

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # Use Persistent Disk path for Render
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'database/agri_ai.db')
+        'sqlite:////var/lib/sqlite/agri_ai.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
     MODEL_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'model/crop_disease_model.h5')
