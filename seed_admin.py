e75107e88dd43b8124c0d09a88ab10f8923a69f5
Digital Pen Tech <digitalpentech@gmail.com>
2026-07-18 12:41:44 +0100

    Security: Refactor admin seeding to use environment variable for password

--- seed_admin.py
+++ seed_admin.py
@@ -1,3 +1,4 @@
+import os
 from factory import create_app
 from factory import db
 from models.models import User
@@ -7,13 +8,16 @@ app = create_app()
 
 def seed_admin():
     with app.app_context():
+        # Get password from environment variable
+        password = os.getenv('ADMIN_PASSWORD', 'default_test_password')
+        
         # Seed Admin User
         if not User.query.filter_by(role='admin').first():
             admin = User(
                 full_name='Administrator',
                 email='admin@agriai.com',
                 phone='0000000000',
-                password_hash=generate_password_hash('adminpassword'),
+                password_hash=generate_password_hash(password),
                 role='admin'
             )
             db.session.add(admin)
